86d883b53adbf3e05bc4c23d68ab9dcc5870c682
wangkai19 <wangkai19@corp.netease.com>
2023-08-31 13:47:16 +0800

    go route test
    Signed-off-by: wangkai19 <wangkai19@corp.netease.com>

--- examples/golang-http/simple/filter.go
+++ examples/golang-http/simple/filter.go
@@ -26,7 +26,7 @@ func (f *filter) sendLocalReplyInternal() api.StatusType {
 // Callbacks which are called in request path
 func (f *filter) DecodeHeaders(header api.RequestHeaderMap, endStream bool) api.StatusType {
 	f.path, _ = header.Get(":path")
-	api.LogDebugf("get path %s", f.path)
+	api.LogInfof("f.config.echoBody2 %s", f.config.echoBody)
 
 	if f.path == "/localreply_by_config" {
 		return f.sendLocalReplyInternal()
